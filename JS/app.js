9c5f182de7d82ace02dc5d29c07574f78796f692
Carolina Giacchi <carogiacchi@gmail.com>
2023-08-08 19:45:44 -0300

    preentrega 1

--- JS/app.js
+++ JS/app.js
@@ -3,23 +3,23 @@ alert(`Bienvenido a Merlot Suites & Studios. Un lugar exclusivo donde podrás vi
 function infoHospedaje() {
     let edificio = prompt(`Dónde te gustaría hospedarte? Responde Suites o Studios`).toLowerCase();
     while(edificio !== `suites` && edificio !== `studios`) {
-        alert(`Debes ingresar un nombre de edificio válido (Suites o Studios).`);
+        console.log(`Debes ingresar un nombre de edificio válido (Suites o Studios).`);
         return infoHospedaje();
     }
     let huespedes = parseInt(prompt(`Indicanos la cantidad de huespedes (mínimo 1, máximo 5)`));
 
     if (huespedes < 1 || huespedes > 5) {
-        alert(`Lo sentimos! No contamos con apartamentos para tantos huespedes.`);
+        console.log(`Lo sentimos! No contamos con apartamentos para tantos huespedes.`);
         return infoHospedaje();
     }
     if (edificio === `suites` && huespedes >= 1 && huespedes <= 2){
-        alert(`Merlot suites se encuentra en Palermo Hollywood. Ofrecemos piscina al aire libre en la terraza, servicios de conserjería y conexión wifi gratuita. Tenemos disponibles 3 tipos de apartamentos, Suite standard, Suite Superior y Suite Deluxe. Comunícate con nosotros para más información!`);
+      alert(`Merlot suites se encuentra en Palermo Hollywood. Ofrecemos piscina al aire libre en la terraza, servicios de conserjería y conexión wifi gratuita. Tenemos disponibles 3 tipos de apartamentos, Suite standard, Suite Superior y Suite Deluxe. Comunícate con nosotros para más información!`);
     } else if (edificio === `suites` && huespedes >= 3 && huespedes <= 5){
-        alert(`Merlot suites se encuentra en Palermo Hollywood. Ofrecemos piscina al aire libre en la terraza, servicios de conserjería y conexión wifi gratuita. Tenemos disponibles 2 tipos de apartamentos, Suite Familiar y  Suite Duplex. Comunícate con nosotros para más información!`);
+      alert(`Merlot suites se encuentra en Palermo Hollywood. Ofrecemos piscina al aire libre en la terraza, servicios de conserjería y conexión wifi gratuita. Tenemos disponibles 2 tipos de apartamentos, Suite Familiar y  Suite Duplex. Comunícate con nosotros para más información!`);
     }else if(edificio === `studios` && huespedes >= 1 && huespedes <= 2) {
-        alert(`Merlot Studios se encuentra en Palermo Soho. Este complejo de apartamentos ofrece un jardin y piscina al aire libre, servicios de conserjería, conexión wifi gratuita y un circuito de spa. Tenemos disponibles 4 tipos de apartamentos, Studio PB con patio, Studio PB con cocina, Studio Standard y Studio Superior. Comunícate con nosotros para más información!`);
+      alert(`Merlot Studios se encuentra en Palermo Soho. Este complejo de apartamentos ofrece un jardin y piscina al aire libre, servicios de conserjería, conexión wifi gratuita y un circuito de spa. Tenemos disponibles 4 tipos de apartamentos, Studio PB con patio, Studio PB con cocina, Studio Standard y Studio Superior. Comunícate con nosotros para más información!`);
     } else if(edificio === `studios` && huespedes >= 3 && huespedes <= 5) {
-        alert(`Merlot Studios se encuentra en Palermo Soho. Este complejo de apartamentos ofrece un jardin y piscina al aire libre, servicios de conserjería, conexión wifi gratuita y un circuito de spa. Tenemos disponible 1 tipo de apartamento, el Studio Triplex. Comunícate con nosotros para más información!`);
+      alert(`Merlot Studios se encuentra en Palermo Soho. Este complejo de apartamentos ofrece un jardin y piscina al aire libre, servicios de conserjería, conexión wifi gratuita y un circuito de spa. Tenemos disponible 1 tipo de apartamento, el Studio Triplex. Comunícate con nosotros para más información!`);
     }
 }
 infoHospedaje();
